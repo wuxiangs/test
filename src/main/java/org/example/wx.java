4ffa0d4308aba3d829413e17a1e7be7f47c54200
wuxiang <1325668250@qq.com>
2021-08-12 13:44:19 +0800

    增加文件wx

---
+++ src/main/java/org/example/wx.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @author wuxiang
+ * @date 2021/8/12 1:40 下午
+ */
+public class wx {
+
+    public static void main(String[] args) {
+        System.out.println("新增");
+    }
+}
